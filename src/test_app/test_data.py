877455f8560208704700a71c5af286da2f8370f3
AlexG <22422193+AlexGTalks@users.noreply.github.com>
2020-12-15 16:32:15 -0600

    add test data example

---
+++ src/test_app/test_data.py
@@ -0,0 +1,39 @@
+"""
+Implement here your test data.
+
+Example:
+
+test_data = [
+            {
+                "code": 422,
+                "json": {
+                    "name": 'alejandro',
+                    "DOB": '2002-03-33',
+                    "gender": Gender.male.name
+                }
+            }, {
+                "code": 422,
+                "json": {
+                    "name": 'alejandro',
+                    "DOB": '2002-03-30',
+                    "gender": "not a gender"
+                }
+            }, {
+                "code": 422,
+                "json": {
+                    "name": '',
+                    "DOB": '2002-03-30',
+                    "gender": "not a gender"
+                }
+            }, {
+                "code": 404,
+                "json": {
+                    "name": 'alejandro',
+                    "DOB": '2002-03-30',
+                    "gender": Gender.male.name,
+                    "movies": [30]
+                }
+            }
+        ]
+
+"""
